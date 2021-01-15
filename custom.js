f3d589307b8134a247515bbefc8f6ba3df596137
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-01-15 12:18:38 -0600

    feat: Cleaned JS

--- custom.js
+++ custom.js
@@ -1,18 +1,10 @@
 let url = "https://fluttering-achieved-syringa.glitch.me/movies"
 
-let test = {
-    title: 'movie title',
-    rating: 4,
-    director: 'Daniel'
-}
-
 const generateMovieDisplay = ({
                                   title,
                                   rating,
                                   director = "No Director listed"
 }) => {
-    // let { title, rating, director } = movie
-
     // make ELEMENTS
     let body = document.createElement('div')
     let firstLine = document.createElement('div')
@@ -38,13 +30,9 @@ const generateMovieDisplay = ({
     body.appendChild(firstLine)
     body.appendChild(directorDisplay)
 
-
-
     document.getElementById('movie').appendChild(body)
 
 }
-
-// generateMovieDisplay(test)
 
 function getMovies() {
     fetch(url).then(function (response) {
@@ -54,10 +42,7 @@ function getMovies() {
         document.getElementById('loading').style.display = 'none'
         console.log(movies)
 
-        // document.getElementById('movie').innerHTML = ''
-
         movies.map(movie => {
-
             generateMovieDisplay(movie)
         })
     }).catch( error => {
@@ -66,31 +51,16 @@ function getMovies() {
         console.error(error)
     });
 }
+
 getMovies()
 
-
-
-
-
-let newMovie = {
-    "title": "The Dark Knight",
-    "rating": "5",
-    "poster": "https://m.media-amazon.com/images/M/MV5BMTk3NDE2NzI4NF5BMl5BanBnXkFtZTgwNzE1MzEyMTE@._V1_SX300.jpg",
-    "year": "2008",
-    "genre": "Action, Thriller, ",
-    "director": "Christopher Nolan",
-    "plot": "Batman struggles to maintain order against the Joker.",
-    "actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Gary Oldman",
-    "id": `${Math.random() * 2000}`
-}
-
-const optionAdd = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(newMovie)
-}
+// const optionAdd = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(newMovie)
+// }
 
 const addMovie = (movie) => {
     const optionAdd = {
@@ -134,11 +104,11 @@ const editMovie = (movie) => {
     })
 }
 
-const optionDelete = {
-    method:'DELETE'
-}
-
 const deleteMovie = (id) => {
+    const optionDelete = {
+        method:'DELETE'
+    }
+
     fetch( url + `/${id}`, optionDelete).then(function(response){
         console.log(response)
 })
@@ -157,8 +127,6 @@ document.getElementById('newMovieSubmit').addEventListener('click', function(e){
         plot
     }
     addMovie(movie)
-
-    // console.log(movie);
 })
 
 // deleteMovie('1337.327659575014')
