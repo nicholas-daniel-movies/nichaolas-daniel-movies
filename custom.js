bce46ea21e2e9693f2b633efd8abd63e9c10b1fe
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-01-15 09:48:32 -0600

    2nd successful response

--- custom.js
+++ custom.js
@@ -1,5 +1,8 @@
 let url = "https://fluttering-achieved-syringa.glitch.me/movies"
 
 fetch(url).then(function(response){
-    console.log(response)
+    // console.log(response)
+    return response.json()
+}).then(function(movies){
+    console.log(movies)
 })
